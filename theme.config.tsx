e885d86361d86c7341d224d45d7360885272a972
marsheltatiana <marsheltatianasanchezvigo@gmail.com>
2024-12-08 15:42:29 -0500

    Actualizo el logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <img src="https://uploads-ssl.webflow.com/627136f58be1570905eb8a28/6458f00e1462140aca5e9f19_mySHOEFITTER%20Logo-p-500.png" width="160px" />,
+  logo: <img src="https://munifilemanager.vercel.app/_next/image?url=%2Fmuni-guadalupe-logo.png&w=256&q=75" width="95px" />,
   project: {
     link: 'https://github.com/myshoefitter/',
   },
